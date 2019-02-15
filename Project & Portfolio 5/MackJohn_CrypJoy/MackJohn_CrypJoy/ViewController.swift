0ac5f1c693a1d2027205d9c8d1baa7c7479b670c
John Mack <jdmack@fullsail.edu>
2019-02-14 21:25:14 -0500

    CREATE: - Created new project for "CrypJoy" Apple Watch application

---
+++ Project & Portfolio 5/MackJohn_CrypJoy/MackJohn_CrypJoy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MackJohn_CrypJoy
+//
+//  Created by Johnny Mack on 2/14/19.
+//  Copyright © 2019 John Mack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
